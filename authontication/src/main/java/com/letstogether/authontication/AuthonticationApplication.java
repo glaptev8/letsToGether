1b106284880fc2747fdaadad3c17f231330a16dc
gleb laptev <gleb.laptev@etraveligroup.com>
2024-03-14 19:45:02 +0100

    Initial commit

---
+++ authontication/src/main/java/com/letstogether/authontication/AuthonticationApplication.java
@@ -0,0 +1,12 @@
+package com.letstogether.authontication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthonticationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthonticationApplication.class, args);
+  }
+}
