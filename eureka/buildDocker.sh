0d5d7e6fb559c7805a315ccdd7ece5500c723d23
Your Name <you@example.com>
2024-04-26 17:14:47 +0200

    ff

--- eureka/buildDocker.sh
+++ eureka/buildDocker.sh
@@ -6,11 +6,13 @@ VERSION="0.0"
 TIMESTAMP=$(date +"%Y%m%d%H%M")
 
 # Сборка и тегирование образа
-docker build -t glaptev8/lets-to-gether-eureka:$VERSION .
+docker build --platform linux/amd64 -t glaptev8/lets-to-gether-eureka:$VERSION .
 docker tag glaptev8/lets-to-gether-eureka:$VERSION glaptev8/lets-to-gether-eureka:${VERSION}-$TIMESTAMP
+docker tag glaptev8/lets-to-gether-eureka:$VERSION glaptev8/lets-to-gether-eureka:latest
 
 # Публикация образов
 docker push glaptev8/lets-to-gether-eureka:$VERSION
 docker push glaptev8/lets-to-gether-eureka:${VERSION}-$TIMESTAMP
+docker push glaptev8/lets-to-gether-eureka:latest
 
 echo "Образы успешно собраны и отправлены на Docker Hub."
